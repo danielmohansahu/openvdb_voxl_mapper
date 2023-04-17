95b629d52f5476a2aa22b4f3791fd72a60b46fc6
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-17 08:26:50 -0400

    Improved test timing.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

--- tests/development.cpp
+++ tests/development.cpp
@@ -5,10 +5,11 @@
 
 // OpenVDB
 #include <openvdb/openvdb.h>
+#include <openvdb/util/CpuTimer.h>
 
 // OVM
 #include <openvdb_voxel_mapper/voxel_cloud.h>
-#include <openvdb_voxel_mapper/operations/ground_plane_extraction.hpp>
+#include <openvdb_voxel_mapper/operations/ground_plane_extraction.h>
 
 // OVM Test
 #include "test_utilities.h"
@@ -21,31 +22,37 @@ void print_middle_of_map(const ovm::Map::MapT& map)
 
 int main(int argc, char** argv)
 {
+  // simple timer
+  openvdb::util::CpuTimer timer;
+
   // construct an empty cloud
   ovm::VoxelCloud cloud;
 
   // generate and add random clouds
   // each iteration _roughly_ correspond to 1 frame of a 128X1024 beam
   //  LIDAR with 120m max range traveling at ~10m/s, for a total of 10 seconds of data
-  std::cout << "Merging clouds into grid..." << std::endl;
-  for (size_t i = 0; i != 10; ++i)
+  timer.start("Merging clouds into grid");
+  for (size_t i = 0; i != 100; ++i)
   {
     const std::string filename = "raw_" + std::to_string(i) + ".vdb";
     cloud.merge(ovm_test::make_random_cloud(filename, 200000, 1.0 * i, 40.0, 0.5 * i, 40.0, 0.0, 10.0));
   }
-  std::cout << "Generated " << cloud.size() << " points." << std::endl;
 
   // extract ground plane from grid (naive single threaded)
-  std::cout << "Extracting ground plane via CPU..." << std::endl;
-  if (const auto map = ovm::ops::ground_plane_extraction_geometric(cloud.grid()); map)
-    print_middle_of_map(map->map);
+  timer.restart("Extracting ground plane via CPU");
+  const auto cpu_map = ovm::ops::ground_plane_extraction_geometric(cloud.grid());
+  timer.stop();
+  if (cpu_map)
+    print_middle_of_map(cpu_map->map);
   else
-    std::cerr << "FAILED TO CONSTRUCT MAP!!!" << std::endl;
+    std::cerr << "FAILED TO CONSTRUCT CPU MAP!!!" << std::endl;
 
   // extract ground plane from grid (GPU)
-  std::cout << "Extracting ground plane via GPU..." << std::endl;
-  if (const auto map = ovm::ops::ground_plane_extraction_geometric_cuda(cloud.grid()); map)
-    print_middle_of_map(map->map);
+  timer.start("Extracting ground plane via GPU");
+  const auto gpu_map = ovm::ops::ground_plane_extraction_geometric_cuda(cloud.grid());
+  timer.stop();
+  if (gpu_map)
+    print_middle_of_map(gpu_map->map);
   else
     std::cerr << "FAILED TO CONSTRUCT MAP!!!" << std::endl;
 
