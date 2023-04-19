f97fc4d2a882ef0c7c587f6a0cb10b34d6088b78
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-19 09:50:53 -0400

    First pass implementation at a ROS1 interface.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

---
+++ include/openvdb_voxel_mapper/conversions/ros1.h
@@ -0,0 +1,58 @@
+/* Conversions to and from OpenVDB types for ROS1.
+ *
+ */
+
+#pragma once
+
+// STL
+#include <optional>
+
+// ROS1
+#include <grid_map_ros/GridMapRosConverter.hpp>
+#include <grid_map_msgs/GridMap.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+// OVM
+#include "../types.h"
+#include "../voxel_cloud.h"
+#include "pcl.h"
+
+namespace ovm::conversions
+{
+
+// construct a ROS PointCloud2 into a VoxelCloud
+template <typename PointT>
+VoxelCloud from_ros(const sensor_msgs::PointCloud2& msg, const Options& opts)
+{  
+  // convert to PCL
+  pcl::PointCloud<PointT> pcl_cloud;
+  pcl::fromROSMsg(msg, pcl_cloud);
+  
+  // construct and return a new VoxelCloud
+  return VoxelCloud(pcl_cloud, opts);
+}
+
+// convert a VoxelCloud into a ROS PointCloud2
+std::optional<sensor_msgs::PointCloud2> to_ros(const VoxelCloud& cloud)
+{
+  // convert to PCL and then to a PointCloud2
+  if (auto pcl_cloud = to_pcl(cloud.grid()); pcl_cloud)
+  {
+    sensor_msgs::PointCloud2 msg;
+    pcl::toROSMsg(*pcl_cloud, msg);
+    return msg;
+  }
+
+  // unable to convert
+  return std::nullopt;
+}
+
+// convert a ovm::Map to a ROS grid map
+std::optional<grid_map_msgs::GridMap> to_ros(const ovm::Map& map, const std::string& layer)
+{
+  // I am a stub.
+  return std::nullopt;
+}
+
+} // namespace ovm::conversions
---
+++ include/openvdb_voxel_mapper/ros1_voxel_cloud.h
@@ -0,0 +1,79 @@
+/* ros1_voxel_cloud.h
+ *
+ * ROS1 wrapper for OVM::VoxelCloud.
+ * 
+ * This class provides API methods for VoxelCloud
+ * allowing for direct interaction to / from ROS
+ * message types.
+ */
+
+// ROS1
+#include <grid_map_msgs/GridMap.h>
+#include <sensor_msgs/PointCloud2.h>
+
+// OVM
+#include "types.h"
+#include "voxel_cloud.h"
+#include "conversions/ros1.h"
+#include "operations/ground_plane_extraction.h"
+
+namespace ovm::ros
+{
+
+class ROS1VoxelCloud
+{
+ public:
+  // expected constructor
+  explicit ROS1VoxelCloud(const Options& options = Options())
+   : _opts(options), _cloud(options) {}
+
+  // merge an incoming sensor_msgs::PointCloud2 cloud
+  template <typename PointT = pcl::PointXYZ>
+  void merge(const sensor_msgs::PointCloud2& msg) { _cloud.merge(conversions::from_ros<PointT>(msg)); }
+  
+  // convert current full cloud to a sensor_msgs::PointCloud2
+  std::optional<sensor_msgs::PointCloud2> cloud() const { return conversions::to_ros(_cloud); }
+
+  // extract the ground plane of the current cloud
+  std::optional<grid_map_msgs::GridMap> ground_plane(const bool gpu = true) const
+  {
+    // perform ground plane extraction via specified method
+    if (auto map = (gpu) ? ops::ground_plane_extraction_geometric_cuda(_cloud.grid())
+                         : ops::ground_plane_extraction_geometric(_cloud.grid())
+        ; map)
+      // conversion succeeded - convert to grid map
+      return conversions::to_ros(*map, "ground");
+    return std::nullopt;
+  }
+
+ public:
+  // pass-through API
+
+  // PASSTHROUGH API: return whether or not we have data
+  bool empty() const { return _cloud.empty(); };
+  
+  // PASSTHROUGH API: return number of available points - this can be expensive.
+  size_t size() const { return _cloud.size(); };
+
+  // PASSTHROUGH API: clear and reset to a blank slate
+  void reset() { _cloud.reset(); };
+
+  // PASSTHROUGH API: write current cloud to file
+  void write(const std::string& filename) const { _cloud.write(filename); };
+
+  // PASSTHROUGH API: remove a single timestamp from the cloud
+  void remove(const float stamp) { _cloud.remove(stamp); };
+  
+  // PASSTHROUGH API: remove all stamps prior to the given, inclusive
+  void remove_before(const float stamp) { _cloud.remove_before(stamp); };
+
+ private:
+  // configuration options
+  // @TODO make shared with underlying cloud
+  Options _opts;
+ 
+  // core underlying voxel cloud (encapsulation > inheritance)
+  VoxelCloud _cloud;
+}; // class ROS1VoxelCloud
+
+} // namespace ovm::ros
