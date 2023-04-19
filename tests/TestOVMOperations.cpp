88fd3d0cbb3c980e613527cbeae7f3b1a9940c21
Daniel M. Sahu <danielmohansahu@gmail.com>
2023-04-19 04:31:28 -0400

    Debugging improvements for Operations tests - not passing.
    Signed-off-by: Daniel M. Sahu <danielmohansahu@gmail.com>

--- tests/TestOVMOperations.cpp
+++ tests/TestOVMOperations.cpp
@@ -26,11 +26,12 @@ TEST_F(TestOVMOperations, testGroundPlane)
 {
   // construct a PCL cloud with hardcoded points
   pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
-  for (int i = -5; i != 5; ++i)
+  for (int i = -5; i != 6; ++i)
   {
     float v = static_cast<float>(i);
     pcl_cloud.push_back({0, 0, v});   // center column of points
-    pcl_cloud.push_back({v, v, 0});   // everywhere else has Z = 0
+    pcl_cloud.push_back({v, v, v});   // diagonals have cascading Z
+    pcl_cloud.push_back({v, -v, v});  // diagonals have cascading Z
   }
 
   // set voxel size to 1.0 meter
@@ -49,15 +50,23 @@ TEST_F(TestOVMOperations, testGroundPlane)
   ASSERT_TRUE(gpu_map);
 
   // compare results
-  EXPECT_TRUE(*cpu_map == *gpu_map);
+  EXPECT_TRUE(cpu_map->pose == gpu_map->pose);
+  EXPECT_TRUE(cpu_map->map == gpu_map->map);
   
   // compare each to ground truth result expectation
   ovm::Map gt_map;
-  gt_map.pose = ovm::Map::PoseT {0,0};
+  gt_map.pose = ovm::Map::PoseT {-5,-5};
   gt_map.map = ovm::Map::MapT {0,0};
 
-  EXPECT_TRUE(*cpu_map == gt_map);
-  EXPECT_TRUE(*gpu_map == gt_map);
+  EXPECT_TRUE(cpu_map->pose == gt_map.pose);
+  EXPECT_TRUE(gpu_map->pose == gt_map.pose);
+  EXPECT_TRUE(cpu_map->map == gt_map.map);
+  EXPECT_TRUE(gpu_map->map == gt_map.map);
+
+  std::cout << "CPU_pose: " << cpu_map->pose << std::endl;
+  std::cout << "CPU_map: \n" << cpu_map->map << std::endl;
+  std::cout << "GPU_pose: " << gpu_map->pose << std::endl;
+  std::cout << "GPU_map: \n" << gpu_map->map << std::endl;
 }
 
 // test label extraction operation
